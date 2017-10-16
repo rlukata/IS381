6bee0a15cbf4c3a2ed74722e3a70a09bdc85a9a0
rlukata@cityuniversity.edu <rlukata@cityuniversity.edu>
2017-10-15 17:42:53 -0700

    Lab2

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace TicTacToe
+{
+    partial class myForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button0x1 = new System.Windows.Forms.Button();
+            this.button0x0 = new System.Windows.Forms.Button();
+            this.flowLayoutPanelBaseLayer = new System.Windows.Forms.FlowLayoutPanel();
+            this.button0x2 = new System.Windows.Forms.Button();
+            this.button1x0 = new System.Windows.Forms.Button();
+            this.button1x1 = new System.Windows.Forms.Button();
+            this.button1x2 = new System.Windows.Forms.Button();
+            this.button2x0 = new System.Windows.Forms.Button();
+            this.button2x1 = new System.Windows.Forms.Button();
+            this.button2x2 = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanelBaseLayer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button0x1
+            // 
+            this.button0x1.AutoSize = true;
+            this.button0x1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button0x1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0x1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0x1.Location = new System.Drawing.Point(109, 3);
+            this.button0x1.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button0x1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button0x1.Name = "button0x1";
+            this.button0x1.Size = new System.Drawing.Size(100, 100);
+            this.button0x1.TabIndex = 0;
+            this.button0x1.UseVisualStyleBackColor = true;
+            this.button0x1.Click += new System.EventHandler(this.button0x0_Click);
+            // 
+            // button0x0
+            // 
+            this.button0x0.AutoSize = true;
+            this.button0x0.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button0x0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0x0.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0x0.Location = new System.Drawing.Point(3, 3);
+            this.button0x0.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button0x0.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button0x0.Name = "button0x0";
+            this.button0x0.Size = new System.Drawing.Size(100, 100);
+            this.button0x0.TabIndex = 1;
+            this.button0x0.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanelBaseLayer
+            // 
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button0x0);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button0x1);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button0x2);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button1x0);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button1x1);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button1x2);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button2x0);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button2x1);
+            this.flowLayoutPanelBaseLayer.Controls.Add(this.button2x2);
+            this.flowLayoutPanelBaseLayer.Location = new System.Drawing.Point(4, 45);
+            this.flowLayoutPanelBaseLayer.MaximumSize = new System.Drawing.Size(450, 450);
+            this.flowLayoutPanelBaseLayer.MinimumSize = new System.Drawing.Size(150, 150);
+            this.flowLayoutPanelBaseLayer.Name = "flowLayoutPanelBaseLayer";
+            this.flowLayoutPanelBaseLayer.Size = new System.Drawing.Size(319, 321);
+            this.flowLayoutPanelBaseLayer.TabIndex = 2;
+            // 
+            // button0x2
+            // 
+            this.button0x2.AutoSize = true;
+            this.button0x2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button0x2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0x2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0x2.Location = new System.Drawing.Point(215, 3);
+            this.button0x2.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button0x2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button0x2.Name = "button0x2";
+            this.button0x2.Size = new System.Drawing.Size(100, 100);
+            this.button0x2.TabIndex = 2;
+            this.button0x2.UseVisualStyleBackColor = true;
+            this.button0x2.Click += new System.EventHandler(this.button0x2_Click);
+            // 
+            // button1x0
+            // 
+            this.button1x0.AutoSize = true;
+            this.button1x0.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1x0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1x0.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1x0.Location = new System.Drawing.Point(3, 109);
+            this.button1x0.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button1x0.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button1x0.Name = "button1x0";
+            this.button1x0.Size = new System.Drawing.Size(100, 100);
+            this.button1x0.TabIndex = 3;
+            this.button1x0.UseVisualStyleBackColor = true;
+            this.button1x0.Click += new System.EventHandler(this.button1x0_Click);
+            // 
+            // button1x1
+            // 
+            this.button1x1.AutoSize = true;
+            this.button1x1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1x1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1x1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1x1.Location = new System.Drawing.Point(109, 109);
+            this.button1x1.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button1x1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button1x1.Name = "button1x1";
+            this.button1x1.Size = new System.Drawing.Size(100, 100);
+            this.button1x1.TabIndex = 4;
+            this.button1x1.UseVisualStyleBackColor = true;
+            this.button1x1.Click += new System.EventHandler(this.button1x1_Click);
+            // 
+            // button1x2
+            // 
+            this.button1x2.AutoSize = true;
+            this.button1x2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1x2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1x2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1x2.Location = new System.Drawing.Point(215, 109);
+            this.button1x2.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button1x2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button1x2.Name = "button1x2";
+            this.button1x2.Size = new System.Drawing.Size(100, 100);
+            this.button1x2.TabIndex = 5;
+            this.button1x2.UseVisualStyleBackColor = true;
+            this.button1x2.Click += new System.EventHandler(this.button1x2_Click);
+            // 
+            // button2x0
+            // 
+            this.button2x0.AutoSize = true;
+            this.button2x0.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2x0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2x0.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2x0.Location = new System.Drawing.Point(3, 215);
+            this.button2x0.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button2x0.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button2x0.Name = "button2x0";
+            this.button2x0.Size = new System.Drawing.Size(100, 100);
+            this.button2x0.TabIndex = 7;
+            this.button2x0.UseVisualStyleBackColor = true;
+            this.button2x0.Click += new System.EventHandler(this.button2x0_Click);
+            // 
+            // button2x1
+            // 
+            this.button2x1.AutoSize = true;
+            this.button2x1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2x1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2x1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2x1.Location = new System.Drawing.Point(109, 215);
+            this.button2x1.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button2x1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button2x1.Name = "button2x1";
+            this.button2x1.Size = new System.Drawing.Size(100, 100);
+            this.button2x1.TabIndex = 8;
+            this.button2x1.UseVisualStyleBackColor = true;
+            this.button2x1.Click += new System.EventHandler(this.button2x1_Click);
+            // 
+            // button2x2
+            // 
+            this.button2x2.AutoSize = true;
+            this.button2x2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2x2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2x2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2x2.Location = new System.Drawing.Point(215, 215);
+            this.button2x2.MaximumSize = new System.Drawing.Size(150, 150);
+            this.button2x2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.button2x2.Name = "button2x2";
+            this.button2x2.Size = new System.Drawing.Size(100, 100);
+            this.button2x2.TabIndex = 9;
+            this.button2x2.UseVisualStyleBackColor = true;
+            this.button2x2.Click += new System.EventHandler(this.button2x2_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(4, 372);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(322, 37);
+            this.buttonReset.TabIndex = 11;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(87, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 33);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // myForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 415);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.flowLayoutPanelBaseLayer);
+            this.Name = "myForm";
+            this.Text = "Tic Tac Toe";
+            this.flowLayoutPanelBaseLayer.ResumeLayout(false);
+            this.flowLayoutPanelBaseLayer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button0x1;
+        private System.Windows.Forms.Button button0x0;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBaseLayer;
+        private System.Windows.Forms.Button button0x2;
+        private System.Windows.Forms.Button button1x0;
+        private System.Windows.Forms.Button button1x1;
+        private System.Windows.Forms.Button button1x2;
+        private System.Windows.Forms.Button button2x0;
+        private System.Windows.Forms.Button button2x1;
+        private System.Windows.Forms.Button button2x2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+
+    public partial class myForm : Form
+    {
+        public myForm()
+        {
+            InitializeComponent();
+        }
+      
+        public enum Player
+        {
+            X,
+            O
+        }
+
+        //initialize player to X 
+        Player currentPlayer = Player.X;
+
+        //event handler for button_click
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Text = currentPlayer.ToString();
+            currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+            button.Enabled = false;
+            Check();
+        }
+
+        private void button0x0_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button0x1_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button1x1_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button2x1_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button1x0_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button2x0_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button1x2_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button0x2_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void button2x2_Click(object sender, EventArgs e)
+        {
+            Button_Click(sender, e);
+        }
+
+        private void disableXOButtonsEndGame()
+        {
+            button0x0.Enabled = false;
+            button0x1.Enabled = false;
+            button0x2.Enabled = false;
+            button1x0.Enabled = false;
+            button1x1.Enabled = false;
+            button1x2.Enabled = false;
+            button2x0.Enabled = false;
+            button2x1.Enabled = false;
+            button2x2.Enabled = false;
+        }
+
+        private void enableXOButtonsEndGame()
+        {
+            button0x0.Enabled = true;
+            button0x1.Enabled = true;
+            button0x2.Enabled = true;
+            button1x0.Enabled = true;
+            button1x1.Enabled = true;
+            button1x2.Enabled = true;
+            button2x0.Enabled = true;
+            button2x1.Enabled = true;
+            button2x2.Enabled = true;
+        }
+
+        private void Check()
+        {
+            //Check if tie
+            if (button0x0.Text != "" && button0x1.Text != "" && button0x2.Text != "" &&
+                button1x0.Text != "" && button1x1.Text != "" && button1x2.Text != "" &&
+                button2x0.Text != "" && button2x1.Text != "" && button2x2.Text != "")
+            {
+                textBox1.Text = "Tied";
+                disableXOButtonsEndGame();
+            }
+
+            //Check diagonal for X
+            if (button0x0.Text == "X" && button1x1.Text == "X" && button2x2.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+
+            }
+            if (button0x2.Text == "X" && button1x1.Text == "X" && button2x0.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            //Check rows for X
+            if (button0x0.Text == "X" && button0x1.Text == "X" && button0x2.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            if (button1x0.Text == "X" && button1x1.Text == "X" && button1x2.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            if (button2x0.Text == "X" && button2x1.Text == "X" && button2x2.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            //Check columns for X
+            if (button0x0.Text == "X" && button1x0.Text == "X" && button2x0.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            if (button0x1.Text == "X" && button1x1.Text == "X" && button2x1.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+            if (button0x2.Text == "X" && button1x2.Text == "X" && button2x2.Text == "X")
+            {
+                textBox1.Text = "Player X wins";
+                disableXOButtonsEndGame();
+            }
+
+            //Check diagonal for O
+            if (button0x0.Text == "O" && button1x1.Text == "O" && button2x2.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                disableXOButtonsEndGame();
+            }
+            if (button0x2.Text == "O" && button1x1.Text == "O" && button2x0.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            //Check rows for O
+            if (button0x0.Text == "O" && button0x1.Text == "O" && button0x2.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            if (button1x0.Text == "O" && button1x1.Text == "O" && button1x2.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            if (button2x0.Text == "O" && button2x1.Text == "O" && button2x2.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            //Check columns for O
+            if (button0x0.Text == "O" && button1x0.Text == "O" && button2x0.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            if (button0x1.Text == "O" && button1x1.Text == "O" && button2x1.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+            if (button0x2.Text == "O" && button1x2.Text == "O" && button2x2.Text == "O")
+            {
+                textBox1.Text = "Player O wins";
+                button0x0.Enabled = false;
+                button0x1.Enabled = false;
+                button0x2.Enabled = false;
+                button1x0.Enabled = false;
+                button1x1.Enabled = false;
+                button1x2.Enabled = false;
+                button2x0.Enabled = false;
+                button2x1.Enabled = false;
+                button2x2.Enabled = false;
+            }
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            button0x0.Text = "";
+            button0x1.Text = "";
+            button0x2.Text = "";
+            button1x0.Text = "";
+            button1x1.Text = "";
+            button1x2.Text = "";
+            button2x0.Text = "";
+            button2x1.Text = "";
+            button2x2.Text = "";
+            buttonReset.Text = "";
+            enableXOButtonsEndGame();
+            currentPlayer = Player.X;
+        }
+    }
+}
